98ab1211dc6a02128ab76aebdc1e489395b19273
Weih Li <weihotline@gmail.com>
2014-11-25 11:49:38 -0800

    Finished Session Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+  before_action :require_signed_in!, only: [:destroy]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      sign_in(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:error] ||= []
+      flash.now[:error] << "Your username or password was incorrect."
+      render :new
+    end
+  end
+
+  def destroy
+    signed_out
+    redirect_to new_session_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+  before_action :require_signed_in!, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
 
+    if @user.save
+      signed_in(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:error] = @user.error.full_messages
+      render :new
+    end
+  end
 
+  def show
+    render json: @user
   end
 
   private
