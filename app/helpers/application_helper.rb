3d1409c7bddf2dd463f4a4d996cb5c5b67e10c50
Weih Li <weihotline@gmail.com>
2014-11-26 10:34:31 -0800

    add choose image button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def header_button_tag(text)
     html = '<button class="btn btn-info btn-lg"'
-    html += 'id="sign_up_button">'
+    html += 'id="sign_up_btn">'
     html += "#{h(text)}</button>"
     html.html_safe
   end
