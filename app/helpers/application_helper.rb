8a4b4124940815024969ba4627ac9a95b4823e5f
Weih Li <weihotline@gmail.com>
2014-11-26 07:10:05 -0800

    refactoring _form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
-  def set_action(url)
-    html = "action=\"#{h(url)}\""
-    html.html_safe
-  end
-
-  def set_value(text)
-    html = "value=\"#{h(text)}\""
-    html.html_safe
-  end
-
   def header_button_tag(text)
     html = '<button class="btn btn-info btn-lg"'
     html += 'id="sign_up_button">'
